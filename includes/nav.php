8549dd79573c03cd769c190741a135598ec95f82
Suzanne Duket <suzanne.duket@gmail.com>
2014-05-12 22:15:25 -0700

    removing dev folder from local machine. I should not have two versions

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
 <div class="navbar">
 	<ul class="nav navbar-nav">
-		<li class="active"><a href="index">Home</a></li>
+		<li><a href="index">Home</a></li>
 		<li><a href="menu">Menus</a></li>
 		<li><a href="news-events">Special Events</a></li>
 	</ul>
