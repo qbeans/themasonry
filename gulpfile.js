8b7708b1c706eb611ae5fe5a1065763b977832fc
Suzanne Duket <suzanne.duket@gmail.com>
2014-12-23 12:56:35 -0800

    configuring gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	browserify = require('gulp-browserify'),
+	compass = require('compass'),
+	concat = require('gulp-concat');
+
+var jsSources = [
+	'components/scripts/template.js'
+]
 
 gulp.task('log', function(){
 	gutil.log('Test log');
+});
+
+gulp.task('js', function() {
+	gulp.src(jsSources)
+		.pipe(concat('_scripts.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/js'))
 });
