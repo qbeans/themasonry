87554575bbfbe34d7e8e212dee27cf27ce109ef7
Suzanne Duket <suzanne.duket@gmail.com>
2014-12-23 15:20:04 -0800

    simplifying index, delete stats folder

--- builds/development/index.php
+++ builds/development/index.php
@@ -1,29 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 
-	<?php
-		require 'includes/head-standard.php';
-		include 'includes/get-menu.php';
-	?>
+	<head>
+		<meta charset="utf-8">
+		<title>The Masonry Seattle - Craft Beer and Woodfire Pizza</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="">
+		<meta name="author" content="">
+
+		<link href="css/bootstrap.css" rel="stylesheet">
+		<link href="css/main-styles.css" rel="stylesheet">
+
+		<link href="http://fonts.googleapis.com/css?family=Roboto+Condensed:700" rel="stylesheet" type='text/css'>
+
+	</head>
+	<body>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+		<script type="html/javascript">
+			//START GOOGLE ANALYTICS CODE
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-42275162-1', 'themasonryseattle.com');
+			ga('send', 'pageview');
+			//END GOOGLE ANALYTICS CODE
+		</script>
 		<div id="wrap">
 			<div id="container">
-				<?php include('includes/nav.php'); ?>
-
 					<div class="row">
-						<div class="col col-sm-12 col-lg-4">
+						<div class="col-lg-12">
 							<h1>The Masonry</h1>
-						</div>
-						<div class="col col-sm-12 col-lg-6">
-							<h2>Craft Beer and Woodfire Pizza</h2>
+							<h2>Craft Beer and Wood Fired Pizza</h2>
 						</div>
 					</div>
 					<div class="row">
 						<div class="col col-sm-4 col-lg-4">
-							<center><img src="img/pizza.jpg" class="img-responsive" alt="The Masonry Pizza" /></center>
+							<center><img src="images/pizza.jpg" class="img-responsive" alt="The Masonry Pizza" /></center>
 						</div>
 						<div class="col col-sm-5 col-lg-5">
 							<h3>About</h3>
-							<p>Specializing in craft beer, wood-fired pizza, and hand selected wines. Come chat with our bartenders and watch our chefs at work. 14 beer taps, 2 cider taps, and 5 wine taps. Located in Uptown at 1st Ave. North and Roy St.</p>
+							<p>Come chat with our bartenders and watch our chefs at work. 14 beer taps, 2 cider taps, and 5 wine taps. Located in Uptown at 1st Ave. North and Roy St.</p>
 						</div>
 						<div class="col col-sm-3 col-lg-3">
 							<h3>Visit</h3>
@@ -42,17 +61,39 @@
 						</div>
 					</div><!--end row -->
 					
-					<div id="menu">
-						
-					</div>
 					<div class="row">
 						<div class="col-lg-12">
-							<h4>For a full list of our bottled beers <a href="menu">visit our menu page</a>.</h4>
+							<h3>Our Menu</h3>
+							<p>Small specialized menu. Seasonal and rotating ingredients. Extensive beer list. 14 beer taps, 2 cider taps, and 5 wine taps.</p>
+							<h4><a href="images/masonry-food-menu.pdf">Click here to download our tap list and bottle list (pdf)</a>.</h4>
+							<h4><a href="images/masonry-food-menu.pdf">Click here to download our food menu (pdf)</a>.</h4>
 						</div>
 					</div>
 
 				</div>
-				<?php include 'includes/footer.php'; ?>
+				<div class="row">
+					<div class="col-lg-12">
+						<h3>Events</h3>
+						<p>For upcoming event information, please see our Facebook page.</p>
+					</div>
+				</div>
+				<footer>
+					<div class="container">
+						<div class="pull-left">
+							<p>20 Roy St. Seattle, WA 98109<br>
+							(206) 453-4375<br>
+							<a href="mailto:info@themasonryseattle.com">info@themasonryseattle.com</a>
+						</p>
+						</div>
+						<div class="pull-right">
+							<p class="text-right">
+								Find us on:<br>
+								<a href="https://www.facebook.com/TheMasonryPizza" target="_blank">Facebook</a><br>
+								<a href="https://twitter.com/TheMasonryPizza" target="_blank">Twitter</a>
+							</p>
+						</div>
+					</div>
+				</footer>
 			</wrap>
 		</body>
 
